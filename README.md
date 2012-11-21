18355bdbad2afd98d5d0997b3d8f401f4047058e
Derek Stobbe <djstobbe@gmail.com>
2012-11-20 22:40:05 -0700

    Hadoken!

---
+++ README.md
@@ -0,0 +1,11 @@
+# ComboBreaker
+
+Does key combos in the browser. Depends on jQuery and stuff.
+
+## Usage
+
+```javascript
+var konamiCode = new ComboBreaker(["up", "up", "down", "down", "left", "right", "left", "right", "b", "a", "enter"], function(){
+    console.log('C-C-C-COMBO BREAKER!');
+});
+```
---
+++ dist/ComboBreaker.js
@@ -0,0 +1,60 @@
+/*! ComboBreaker - v0.1.0 - 2012-11-20
+* https://github.com/Problematic/ComboBreaker
+* Copyright (c) 2012 Derek Stobbe; Licensed MIT */
+
+(function ($) {
+    "use strict";
+
+    var ComboBreaker = function (combo, callback, options) {
+        var index = 0, timeout = null;
+
+        if (!(this instanceof ComboBreaker)) { return new ComboBreaker(combo, callback, options); }
+
+        this.options = $.extend({}, {
+            keyMap: {
+                'enter':    13,
+                'esc':      27,
+                'left':     37,
+                'up':       38,
+                'right':    39,
+                'down':     40
+            },
+            resetTime: 1000,
+            resetOnMistake: true,
+            stepCallback: null,
+            target: document
+        }, options);
+        this.combo = combo;
+        this.callback = callback;
+
+        $(this.options.target).keyup(function (e) {
+            // for some reason, jQuery always gives us back the upper-case
+            // ASCII value for e.which, which brings us to:
+            var expected = this.options.keyMap[this.combo[index]] ||
+                this.combo[index].toUpperCase().charCodeAt();
+
+            clearTimeout(timeout);
+
+            if (e.which === expected) {
+                if (typeof this.options.stepCallback === "function") {
+                    this.options.stepCallback(this.combo, this.combo[index]);
+                }
+                if (index === this.combo.length - 1) {
+                    this.callback(this.combo);
+                    index = 0;
+
+                    return;
+                }
+
+                index++;
+                timeout = setTimeout(function () {
+                    index = 0;
+                }.bind(this), this.options.resetTime);
+            } else if (this.options.resetOnMistake === true) {
+                index = 0;
+            }
+        }.bind(this));
+    };
+
+    window.ComboBreaker = ComboBreaker;
+}(window.jQuery));
